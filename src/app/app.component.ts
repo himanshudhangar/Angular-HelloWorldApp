556c1d49955a7debd3cdbbeecdaf1333e9b42ab6
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-11-16 20:53:37 +0530

    Added:Opening a new tab using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  imgUrl="";
+  url: string | URL | undefined;
+  ngOnInit():void{
+    this.title="Hello from Bridgelabz"
+    this.imgUrl="assets/images/BridgeLabzImage.jpg";
+    this.url="/assets/Bridgelabzz.jpg";
+  }
+  onClick($event: any){
+    console.log("Save button is clicked",$event);
+    window.open(this.url,"_blank")
+  }
 }
