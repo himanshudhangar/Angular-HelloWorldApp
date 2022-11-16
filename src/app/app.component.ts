baff62ab0370ae4e88e269aa7032b5e5f4a25567
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-11-16 21:13:10 +0530

    Added:Regex using Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'demo';
   imgUrl="";
   url: string | URL | undefined;
+  userName:string="";
+  nameError:string="";
   ngOnInit():void{
     this.title="Hello from Bridgelabz"
     this.imgUrl="assets/images/BridgeLabzImage.jpg";
@@ -17,5 +19,15 @@ export class AppComponent {
   onClick($event: any){
     console.log("Save button is clicked",$event);
     window.open(this.url,"_blank")
+  }
+  onInput($event:any){
+    console.log("Getting Input");
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!"
   }
 }
